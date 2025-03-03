4f02e757b893d22b2c6f293442c8fc5fc6e2fda6
Mrinal Patra <96857582+mrinal2002@users.noreply.github.com>
2025-03-03 20:12:04 +0530

    [Mrinal] desc: calculate mean height of football team players

---
+++ MeanHeight.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MeanHeight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0.0;
+
+        for (int i = 0; i < 11; i++) {
+            System.out.print("Enter height of player " + (i + 1) + ": ");
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+
+        System.out.println("Mean height: " + (sum / 11));
+    }
+}
