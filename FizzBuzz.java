39a9e35e5a236b4d8c5b50eb7517a1da60cd9e84
Mrinal Patra <96857582+mrinal2002@users.noreply.github.com>
2025-03-03 20:21:00 +0530

    [Mrinal] desc: FizzBuzz program to replace multiples

---
+++ FizzBuzz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        String[] results = new String[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 15 == 0) results[i] = "FizzBuzz";
+            else if (i % 3 == 0) results[i] = "Fizz";
+            else if (i % 5 == 0) results[i] = "Buzz";
+            else results[i] = String.valueOf(i);
+        }
+
+        for (int i = 1; i <= num; i++) System.out.println("Position " + i + " = " + results[i]);
+    }
+}
